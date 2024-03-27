fd340d1d91511a3d302e4dc0bad0760fcdc35a06
silegle <3418793932@qq.com>
2024-03-27 17:35:22 +0800

    添加项目文件。

---
+++ cal.h
@@ -0,0 +1,47 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<assert.h>
+typedef struct node//压栈和出栈都在栈顶进行(这里的栈顶指前一段)
+{
+    char val;//数据
+    struct node* next;//指针
+}pnode;
+typedef struct seqstack
+{
+    int size;//记录栈的大小
+    pnode* top;//指向栈顶元素
+}phead;
+
+phead* initstack();//创建栈
+int isempty(phead* istack);
+pnode* seqstack_top(phead* istack);
+pnode* seqstack_pop(phead* istack);
+void seqstack_push(phead* istack, char x);
+char buffer[256];//即对数组中每个数据都初始化为'\0'(\0的ascill码是0)
+void char_put(char ch);
+int priority(char ch);
+int is_number(char ch);
+int is_operator(char ch);
+int is_left(char ch);
+int is_right(char ch);
+int transform(char str[]);
+
+typedef struct node1//这里的栈是整型栈
+{
+    int val;//数据
+    struct node1* next;//指针
+}pnode1;
+typedef struct seqstack1
+{
+    int size;//记录栈的大小
+    pnode1* top;//指向栈顶元素
+}phead1;
+phead1* initstack1();
+int isempty1(phead1* istack);
+int seqstack_top1(phead1* istack);
+int seqstack_pop1(phead1* istack);
+void seqstack_push1(phead1* istack, int x);
+int express(int left, int right, char op);
+int getsize(phead1* stack);
+int calculate(char str[]);
---
+++ test.c
@@ -0,0 +1,30 @@
+﻿#include"cal.h"
+int main()
+{
+    char str[1024] = { 0 };//将数组每个元素赋值为'\0'
+    printf("请输入四则运算表达式:\n");
+    scanf_s("%s", str, 20);
+    int number = transform(str);
+    if (number == -1)
+    {
+        printf("表达式转换错误:\n");
+    }
+    else if (number == 1)
+    {
+        printf("转化后的表达式: %s\n", buffer);
+    }
+    else
+    {
+        return 0;
+    }
+    //成功换成后缀表达式
+
+
+    //下方计算后缀表达式
+    int num = calculate(buffer);
+    if (num == 999)
+    {
+        return 0;
+    }
+    printf("%d\n", num);
+}
